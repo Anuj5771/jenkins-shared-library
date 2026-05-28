45f59db36ac7733ba8b7384eee5e8d9b1e02658d
Anuj yadav <anujpbh2022@gmail.com>
2026-05-28 19:21:01 +0530

    Update DeploymentManager.groovy

--- src/org/company/DeploymentManager.groovy
+++ src/org/company/DeploymentManager.groovy
@@ -4,10 +4,12 @@ class DeploymentManager {
 
     def steps
     String environment
+    String deploymentType
 
-    DeploymentManager(steps, String environment) {
+    DeploymentManager(steps, String environment, String deploymentType) {
         this.steps = steps
         this.environment = environment
+        this.deploymentType = deploymentType
     }
 
     def validate() {
@@ -27,6 +29,20 @@ class DeploymentManager {
         }
         else {
             steps.echo "Invalid environment"
+            return
+        }
+
+        if(deploymentType == "rolling") {
+            steps.echo "Using Rolling Deployment Strategy"
+            steps.echo "Updating servers one by one"
+        }
+        else if(deploymentType == "blue-green") {
+            steps.echo "Using Blue-Green Deployment Strategy"
+            steps.echo "Deploying to GREEN environment"
+            steps.echo "Switching traffic from BLUE to GREEN"
+        }
+        else {
+            steps.echo "Invalid deployment type"
         }
     }
 
